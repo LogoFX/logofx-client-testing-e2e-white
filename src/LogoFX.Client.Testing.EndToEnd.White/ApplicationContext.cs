d9c8be27b569dc5393bebf9fc6fd5002dc8ba91a
godrose <godrose@gmail.com>
2016-03-19 10:17:19 +0200

    Added solution.

---
+++ src/LogoFX.Client.Testing.EndToEnd.White/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using TestStack.White;
+
+namespace LogoFX.Client.Testing.EndToEnd.White
+{      
+    /// <summary>
+    /// Ambient Context for <see cref="TestStack.White.Application"/>
+    /// </summary>
+    public static class ApplicationContext
+    {
+        /// <summary>
+        /// Gets or sets the application.
+        /// </summary>
+        /// <value>
+        /// The application.
+        /// </value>
+        public static Application Application { get; set; }
+    }
+}
