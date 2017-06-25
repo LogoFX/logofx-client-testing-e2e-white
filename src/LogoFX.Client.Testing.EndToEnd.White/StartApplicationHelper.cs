74acfdbda162b55229d2ec3558cf396684fef0c5
Gennady Verdel <godrose@gmail.com>
2017-06-25 10:46:45 +0300

    Moved Start Application Service to the core package

--- src/LogoFX.Client.Testing.EndToEnd.White/StartApplicationHelper.cs
+++ src/LogoFX.Client.Testing.EndToEnd.White/StartApplicationHelper.cs
@@ -1,17 +1,18 @@
-﻿using TestStack.White;
+﻿using LogoFX.Client.Testing.Contracts;
+using TestStack.White;
 
 namespace LogoFX.Client.Testing.EndToEnd.White
-{
+{   
     /// <summary>
     /// Helper class for starting an application using <see cref="TestStack.White"/> framework
     /// </summary>
-    public static class StartApplicationHelper
+    public class StartApplicationHelper : IStartApplicationHelper
     {
         /// <summary>
         /// Starts the application.
         /// </summary>
         /// <param name="startupPath">The startup path.</param>
-        public static void StartApplication(string startupPath)
+        public void StartApplication(string startupPath)
         {
             ApplicationContext.Application = Application.Launch(startupPath);
             ApplicationContext.Application.WaitWhileBusy();
