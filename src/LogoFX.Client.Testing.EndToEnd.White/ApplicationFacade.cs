20f24451d765ba89542f6cc3a406fe0f65da044d
Gennady Verdel <godrose@gmail.com>
2017-07-19 12:18:41 +0300

    Updated dependent packages.

---
+++ src/LogoFX.Client.Testing.EndToEnd.White/ApplicationFacade.cs
@@ -0,0 +1,29 @@
+﻿using LogoFX.Client.Testing.Contracts;
+using TestStack.White;
+
+namespace LogoFX.Client.Testing.EndToEnd.White
+{   
+    /// <summary>
+    /// Helper class for starting an application using <see cref="TestStack.White"/> framework
+    /// </summary>
+    public class ApplicationFacade : IApplicationFacade
+    {
+        /// <summary>
+        /// Starts the application.
+        /// </summary>
+        /// <param name="startupPath">The startup path.</param>
+        public void Start(string startupPath)
+        {
+            ApplicationContext.Application = Application.Launch(startupPath);
+            ApplicationContext.Application.WaitWhileBusy();
+        }
+
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public void Stop()
+        {
+            ApplicationContext.Application?.Dispose();
+        }
+    }
+}
