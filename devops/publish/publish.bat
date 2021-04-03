d05b3bb0e0eeff657fd3d2c1ec41a3369dd60679
Gennady Verdel <godrose@gmail.com>
2021-04-03 07:12:26 +0300

    devops: added means for package creation and storage #6

--- devops/publish/publish.bat
+++ devops/publish/publish.bat
@@ -1,6 +1,10 @@
-cd ../build
-call build.bat
+SET package_name=LogoFX.Client.Testing.EndToEnd.White
+SET package_version=2.2.0-rc2
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '../build/build.ps1'"
 cd ../pack
 call ./pack.bat
 cd ../publish
-call ./copy.bat LogoFX.Client.Testing.EndToEnd.White 2.2.0-rc2 %1
+call ./copy.bat %package_name% %package_version% %1
+cd ../install
+call uninstall-global-single.bat %package_name% %package_version%
+cd ..
