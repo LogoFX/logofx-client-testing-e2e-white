0d375d8f4e95721db46c9004e0d60541d8cd31e0
Gennady Verdel <godrose@gmail.com>
2021-04-09 17:02:26 +0300

    chore: files cleanup

--- src/LogoFX.Client.Testing.EndToEnd.White/netframework/Properties/AssemblyInfo.cs
+++ src/LogoFX.Client.Testing.EndToEnd.White/netframework/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LogoFX.Client.Testing.EndToEnd.White")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright �  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,8 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.2.0")]
 [assembly: AssemblyFileVersion("2.2.0")]
